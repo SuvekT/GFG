f025d9d21380dbd69e42d67f447af6aa86ab3e80
Suvek Thange <98460649+SuvekT@users.noreply.github.com>
2022-08-29 22:48:12 +0530

    Time: 10 ms (58.97%), Space: 14 MB (6.61%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        if(cost.size()==2){
+            return min(cost[0], cost[1]);
+        }
+        vector<int> dp;
+        dp.push_back(cost[0]);
+        dp.push_back(cost[1]);
+        for (int i=2; i<cost.size(); i++){ 
+            dp.push_back(cost[i] + min(dp[i-1], dp[i-2]));  
+        }
+        return min(dp.back(), dp[dp.size() - 2]);
+    }
+};

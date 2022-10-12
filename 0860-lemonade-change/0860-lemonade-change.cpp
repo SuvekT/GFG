62571974c2f7708cff27a352740f6b64e274b2e0
Suvek Thange <98460649+SuvekT@users.noreply.github.com>
2022-10-12 23:01:03 +0530

    Time: 149 ms (74.24%), Space: 83.5 MB (7.20%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives=0,tens=0;
+        for(int  i=0;i<bills.size();i++){
+            if(bills[i]==5) fives++;
+            // 2nd case
+           else if(bills[i]==10){
+               if(fives==0) return false;
+                tens++;
+                 fives--;
+            }
+            
+            // 3rd case
+            else{
+                if(fives>0 && tens>0){
+                    fives--,tens--;
+                }
+                else if(fives>=3) fives-=3;
+                else return false;
+                
+            }
+        }
+        return true;
+    }
+};

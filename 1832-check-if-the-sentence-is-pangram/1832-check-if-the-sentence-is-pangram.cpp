79f769bf1514ae6ee55b6c71c97026765cad2b56
Suvek Thange <98460649+SuvekT@users.noreply.github.com>
2022-10-17 10:00:09 +0530

    Time: 10 ms (14.30%), Space: 6.4 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
-    bool checkIfPangram(string sentence) {
+    bool checkIfPangram(string s) {
+        if(s.size()<26) return false;
+        
+        /*
         map<char,int> mp;
         for(auto it:sentence) mp[it]++;
-        return (mp.size()==26) ? true : false ; 
-    }
+        return (mp.size()==26) ? true : false ; */
+        unordered_set<char> st;
+        for(auto it:s) st.insert(it);
+        return (st.size()==26) ? true : false ;
+    } 
 };
